42c3f19af2bf509f299c40a82705cf934378c1a7
Thomas ZABALO <150163380+Thomas-Zabalo@users.noreply.github.com>
2025-01-14 12:52:23 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+// Écrans de l'application
+import HomeScreen from './src/pages/HomeScreen';
+import SplashScreen from './src/pages/SplashScreen';
+import Introslider from './src/pages/Introslider';
+
+
+const Stack = createStackNavigator();
+
+// Fonction principale de l'application
+export default function App() {
+  const [initialRoute] = useState('Splash');
+
+  if (!initialRoute) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={initialRoute}>
+        <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Intro" component={Introslider} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
