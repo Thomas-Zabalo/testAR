da0a7a9275100d80550e201849ac1bdad3de4f27
Thomas ZABALO <thomas.zabalo@gmail.com>
2025-01-15 16:41:16 +0100

    add android

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,12 +1,9 @@
 import React, { useRef } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
-import { GLView } from 'expo-gl';
 import { CameraView, useCameraPermissions } from 'expo-camera';
-import * as THREE from 'three';
 
 
-export default function Home({ navigation }) {
-  const cameraRef = useRef(null);
+export default function Home() {
   const [permission, requestPermission] = useCameraPermissions();
 
   if (!permission) {
@@ -22,37 +19,10 @@ export default function Home({ navigation }) {
     );
   }
 
-  async function onContextCreate(gl) {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000);
-    camera.position.z = 2;
-
-    const renderer = new THREE.WebGLRenderer({ canvas: gl.canvas, context: gl });
-    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
-
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    const render = () => {
-      requestAnimationFrame(render);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-      gl.endFrameEXP();
-    };
-
-    render();
-  }
-
-
-
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} ref={cameraRef} />
-      <GLView style={styles.glview} onContextCreate={onContextCreate} />
+      <CameraView style={styles.camera} />
     </View>
   );
 }
